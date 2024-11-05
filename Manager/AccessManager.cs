95cf4c2d0edb81e30f36daa8fd83e7c7e681757d
nerthul11 <leandrorositto92@gmail.com>
2024-11-05 09:30:07 -0300

    Hotfix to always initiate AccessModule

--- Manager/AccessManager.cs
+++ Manager/AccessManager.cs
@@ -1,6 +1,9 @@
+using AccessRandomizer.Settings;
+using AccessRandomizer.Modules;
+using ItemChanger;
 using Newtonsoft.Json;
 using RandomizerMod.Logging;
-using AccessRandomizer.Settings;
+using RandomizerMod.RC;
 
 namespace AccessRandomizer.Manager
 {
@@ -13,6 +16,12 @@ namespace AccessRandomizer.Manager
             ConnectionMenu.Hook();
             LogicHandler.Hook();
             SettingsLog.AfterLogSettings += AddFileSettings;
+            RandoController.OnExportCompleted += InitiateModule;
+        }
+
+        private static void InitiateModule(RandoController controller)
+        {
+            ItemChangerMod.Modules.GetOrAdd<AccessModule>();
         }
 
         private static void AddFileSettings(LogArguments args, System.IO.TextWriter tw)
@@ -21,7 +30,7 @@ namespace AccessRandomizer.Manager
             tw.WriteLine("Access Randomizer Settings:");
             using JsonTextWriter jtw = new(tw) { CloseOutput = false };
             RandomizerMod.RandomizerData.JsonUtil._js.Serialize(jtw, Settings);
-            tw.WriteLine();
+            tw.WriteLine();            
         }        
     }
 }
